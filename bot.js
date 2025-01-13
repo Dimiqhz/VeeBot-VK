ee0015a0065a79ae7e7e7796157a35be712b6ba7
Dimiqhz <dimiqhz@gmail.com>
2025-01-13 07:46:42 +0300

    Updated sctructure for statistic module

--- bot.js
+++ bot.js
@@ -2,4 +2,6 @@ import { VK } from 'vk-io';
 
 const vk = new VK({
     token: text
-})
+});
+
+// Connect modules
---
+++ modules/stats.js
@@ -0,0 +1,13 @@
+// THERE IS NOTHING HERE
+
+function on_enable(){
+
+}
+
+function send_stats(){
+
+}
+
+function send_error_info(){
+
+}
